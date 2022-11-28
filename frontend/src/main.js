09ed6aa84ae5a322e91a8150f9b4777288489f31
George Malki <george.malki@senarion.com>
2022-11-28 19:09:50 +0100

    Installed primevue

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,15 @@
 import { createApp } from "vue/dist/vue.esm-bundler";
 import router from "@/router";
 import App from "./App.vue";
+import PrimeVue from "primevue/config";
 
-createApp(App).use(router).mount("#app");
+import PrimeDialog from "primevue/dialog";
+
+const app = createApp(App);
+
+app.use(router);
+app.use(PrimeVue);
+
+app.component("PrimeDialog", PrimeDialog);
+
+app.mount("#app");
